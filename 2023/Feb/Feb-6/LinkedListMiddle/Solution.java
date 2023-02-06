e6d1f1b86c05b9aaf23b0c525b069f8a7c0aa644
OAtulA <oraonatula@gmail.com>
2023-02-07 00:06:17 +0530

    Modified LinkedListMiddle/Solution.java with 2 pointer approach.

--- 2023/Feb/Feb-6/LinkedListMiddle/Solution.java
+++ 2023/Feb/Feb-6/LinkedListMiddle/Solution.java
@@ -1,56 +1,53 @@
 // Node of a linked list
- class Node {
-   int data;
+class Node {
+    int data;
     Node next;
-    Node(int d)  { data = d;  next = null; }    
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
 }
 
-
-public class Solution
-{
+public class Solution {
     public static void main(String[] args) {
-        
-        Node head=  new Node(1);
+
+        Node head = new Node(1);
         Node tail = head;
         for (int i = 2; i < 6; i++) {
             Node n = new Node(i);
-            if(head.equals(tail) ){
+            if (head.equals(tail)) {
                 tail = new Node(i);
                 head.next = tail;
-            }
-            else{
-                tail.next= n;
+            } else {
+                tail.next = n;
                 tail = n;
             }
-                
+
         }
 
-        int ans=  Solution.getMiddle(head);
-        System.out.println("Ans: "+ ans);
+        int ans = Solution.getMiddle(head);
+        System.out.println("Ans: " + ans);
 
     }
 
-    public static int getMiddle(Node head)
-    {
-        int count =0, ans =-1;
-        
-        Node itr = head;
-        
-        while(itr.next !=  null){
-            itr = itr.next;
-            count++;
+    public static int getMiddle(Node head) {
+        //int count = 0, ans = -1;
+
+        Node fast = head, slow = head;
+        /*
+         * while(itr.next != null){
+         * itr = itr.next;
+         * count++;
+         * }
+         * System.out.println("num of elements: "+count);
+         */
+        slow = head;
+        while(head != null && head.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        System.out.println("num of elements: "+count);
-        itr = head;
-        for(int i =0; i< count/2 ; i++ )
-        {
-            
-            itr =itr.next;            
-        }
-        ans = itr.data;
-        
-        
-        return ans;
+        return slow.data;
     }
 
 }
