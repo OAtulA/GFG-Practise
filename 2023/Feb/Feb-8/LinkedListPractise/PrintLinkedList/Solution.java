05c6a63af565a2dd93defaf012609261bb385191
OAtulA <oraonatula@gmail.com>
2023-02-08 23:53:54 +0530

    SOlution works fine just needs some formatting.

---
+++ 2023/Feb/Feb-8/LinkedListPractise/PrintLinkedList/Solution.java
@@ -0,0 +1,26 @@
+// Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
+class Solution
+{
+    // Print elements of a linked list on console
+    // head pointer input could be NULL as well
+    // for empty list
+    void display(Node head)
+    {
+        System.out.print("{");
+        while(head!= null){
+            System.out.print(head.data);
+            head = head.next;
+            if(head != null) System.out.print(",");
+        }
+        System.out.print("}");
+    }
+}
