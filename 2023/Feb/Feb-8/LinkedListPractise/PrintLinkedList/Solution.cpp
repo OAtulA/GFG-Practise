ad9e673e60a7a3d3fc29c329aaf070dde4d5c47b
OAtulA <oraonatula@gmail.com>
2023-02-09 00:02:32 +0530

    Added a C++ solution of same.

---
+++ 2023/Feb/Feb-8/LinkedListPractise/PrintLinkedList/Solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  */
+  struct Node
+  {
+     int data;
+     Node *next;
+  };
+
+
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      while (head != NULL)
+      {
+        cout<<head->data<<" " ;
+        head = head->next;
+      }
+      
+    }
+};
+
