f9ab4294ebf17c3b8f269503dd06e883f0f6fa23
OAtulA <oraonatula@gmail.com>
2023-02-10 20:24:36 +0530

    Starting to have hands on with the Array in Java.

---
+++ 2023/Feb/Feb-9/BasicsOfJavaArrays/CloneArray.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class CloneArray {
+    public static void main(String[] args) throws IOException {
+        
+        int[] arr = new int[5];
+        for (int i = 1; i < 6; i++) {
+            arr[i-1] = i;
+        }
+        System.out.println("Array: "+ Arrays.toString(arr));
+        int[] arr2 = arr.clone();
+
+
+        System.out.println("New array copied: "+ arr2);
+        System.out.println("arr ==arr2: "+ (arr == arr2));
+    }
+}
