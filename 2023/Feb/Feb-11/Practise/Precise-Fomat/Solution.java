30f1d1ff0aac96de13b442bb94a3fb0819f44897
OAtulA <oraonatula@gmail.com>
2023-02-12 00:17:53 +0530

    Edited Precise-Fomat/Solution.java

--- 2023/Feb/Feb-11/Practise/Precise-Fomat/Solution.java
+++ 2023/Feb/Feb-11/Practise/Precise-Fomat/Solution.java
@@ -2,6 +2,8 @@ class Geeks{
     
     static void printInFormat(float a, float b){
         float result = a/b;
+
+        System.out.format("%f %.3f", result, result);
         
         
         // Your code here to print upto 3 decimal places
