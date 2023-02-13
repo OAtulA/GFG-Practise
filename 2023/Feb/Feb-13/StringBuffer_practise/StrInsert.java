941b71075a4a9d848ed2f4efb66b62086b9e4ccb
OAtulA <oraonatula@gmail.com>
2023-02-14 01:33:49 +0530

    I tried insert method.

--- 2023/Feb/Feb-13/StringBuffer_practise/StrInsert.java
+++ 2023/Feb/Feb-13/StringBuffer_practise/StrInsert.java
@@ -3,6 +3,14 @@ import java.io.*;;
 public class StrInsert {
     
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuffer s = new StringBuffer("Wo hoho  Gill");
+        System.out.print("Enter a first name: ");
+        String Line1 =  input.readLine();
+        s.insert( "Wo hoho ".length(), Line1 );
+        System.out.println();
+        System.out.println("THis is final thing:  "+s);
+
     }
 }
