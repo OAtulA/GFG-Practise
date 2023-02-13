eb71807e1fd2bfdb45a3d59d5d061c3cbb5a6d5c
OAtulA <oraonatula@gmail.com>
2023-02-14 00:53:26 +0530

    Making the StringBuffer variable empty.

--- 2023/Feb/Feb-13/StringBuffer_practise/StrAppend.java
+++ 2023/Feb/Feb-13/StringBuffer_practise/StrAppend.java
@@ -1,13 +1,20 @@
+import java.io.*;
 public class StrAppend {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer s = new StringBuffer();
-        
+
         // Practised the most basic variant of append
         for (int i = (int)'A'; i <= (int)'Z'; i++) {
             s.append( (char)i);
         }
         System.out.println("Final String: "+s);
+        
+        //Empty the StringBuffer.
+        s = new StringBuffer("");
+
+        // Appending the input
+
 
 
     }
