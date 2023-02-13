637dba91bdf1b8c2c8a4b2d7b3dc0bc9773cf216
OAtulA <oraonatula@gmail.com>
2023-02-14 00:50:27 +0530

    Adding some comment to make it look better.

--- 2023/Feb/Feb-13/StringBuffer_practise/StrAppend.java
+++ 2023/Feb/Feb-13/StringBuffer_practise/StrAppend.java
@@ -2,10 +2,13 @@ public class StrAppend {
     public static void main(String[] args) {
         
         StringBuffer s = new StringBuffer();
-
+        
+        // Practised the most basic variant of append
         for (int i = (int)'A'; i <= (int)'Z'; i++) {
             s.append( (char)i);
         }
         System.out.println("Final String: "+s);
+
+
     }
 }
