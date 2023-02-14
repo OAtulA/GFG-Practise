df655a911744405ec1474dd13c1961af4cfd44ae
OAtulA <oraonatula@gmail.com>
2023-02-15 00:09:19 +0530

    Practised StringBuffer_practise/str_reverse.java gone nice

--- 2023/Feb/Feb-13/StringBuffer_practise/str_reverse.java
+++ 2023/Feb/Feb-13/StringBuffer_practise/str_reverse.java
@@ -14,8 +14,14 @@ public class str_reverse {
         s2.reverse();
 
         System.out.println("StringBuffer now: "+ s2);
+        System.out.println();
 
         // Since here we can't see any changes let's try something else.
+
+        StringBuffer s3  = new StringBuffer("SOme more text in here.");
+        System.out.println("Initial text: "+s3);
+        s3.reverse();
+        System.out.println("Operated StringBuffer: "+ s3);
 
     }
 }
