d55dc35b70acd80e8dcf64d538dc9e5ca81ac74f
OAtulA <oraonatula@gmail.com>
2023-02-15 00:25:52 +0530

    Added StringBuffer_practise/str_setcharAt.java

---
+++ 2023/Feb/Feb-13/StringBuffer_practise/str_setcharAt.java
@@ -0,0 +1,8 @@
+public class str_setcharAt {
+    
+    public static void main(String[] args) {
+        StringBuffer s = new StringBuffer("Boom man.");
+
+        System.out.println("Initially: "+s);
+    }
+}
